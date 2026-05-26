2d311cd41daa26c17670f6d499f60c41e3912ec0
Lpxmari <mariana.ramirezc1@uqvirtual.edu.co>
2026-05-26 15:53:32 -0500

    Ajustes para despliegue

--- src/main/java/co/edu/uniquindio/proyectoavanzada/security/SecurityConfig.java
+++ src/main/java/co/edu/uniquindio/proyectoavanzada/security/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("https://portal-educativo-59ae4.web.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
